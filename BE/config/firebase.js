968b94d558ab3f909838fc3c61adc78bea8a7434
dat-nguyen37 <dat958734@gmail.com>
2026-05-05 00:00:56 +0700

    .

--- BE/config/firebase.js
+++ BE/config/firebase.js
@@ -2,10 +2,11 @@
 const admin = require("firebase-admin");
 const path = require("path");
 require("dotenv").config();
-const serviceAccountPath = path.resolve("./account_service.json");
+const serviceAccount = JSON.parse(process.env.GCP_SERVICE_ACCOUNT);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 admin.initializeApp({
-  credential: admin.credential.cert(require(serviceAccountPath)),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
